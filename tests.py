1fb7132d6dce5008a1df0530e5a0872c8d2d441e
Tom Wardill <tom@howrandom.net>
2011-02-24 11:44:49 +0000

    Start of some tests, and a way to run them

---
+++ tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+class ConfigTests(unittest.TestCase):
+    """ Test the config is generated normally """
+
+    def setUp(self):
+        pass
+
+    def testPass(self):
+        self.assertTrue(True)
